b5c31ca9bfed70973362f9aee48cf3af37fc5a93
ThanakornWongklad <102203038+ThanakornWonglad@users.noreply.github.com>
2023-08-22 22:35:56 +0700

    Delete int221_backend/int221 directory

--- int221_backend/int221/src/main/java/sit/int221/repositories/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package sit.int221.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sit.int221.entities.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
--- int221_backend/int221/src/main/java/sit/int221/utils/AnnouncementDisplay.java
+++
@@ -1,5 +0,0 @@
-package sit.int221.utils;
-
-public enum AnnouncementDisplay {
-    Y, N
-}
