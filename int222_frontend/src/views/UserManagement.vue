9c10f3f296c05c4600113ccf8029bfdf04bcdf69
tana <tanasit.istg@mail.kmutt.ac.th>
2023-08-25 22:54:17 +0700

    [FRONTEND] fea: now you can actually add or edit user

--- int222_frontend/src/views/UserManagement.vue
+++ int222_frontend/src/views/UserManagement.vue
@@ -1,7 +1,7 @@
 <script setup>
 import NavbarComponent from '../components/NavbarComponent.vue'
 import { getUserById } from '../composable/getData.js'
-import { formatDatetimeLocal } from '../composable/formatDatetime';
+import { formatDatetimeLocalUTC } from '../composable/formatDatetime';
 import { useRoute, useRouter } from 'vue-router';
 import Swal from 'sweetalert2'
 import { ref, onMounted } from 'vue';
@@ -51,6 +51,71 @@ const showBackButtonConfirmation = () => {
         }
     })
 }
+
+const AddEditUser = async () => {
+    const data = {
+        username: user.value.username,
+        name: user.value.name,
+        email: user.value.email,
+        role: user.value.role
+    }
+
+    if (isAddUserPage.value !== true) {
+        const response = await fetch(import.meta.env.VITE_ROOT_API + "/api/users/" + params.id, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+
+        if (response.ok) {
+            Swal.fire({
+                icon: 'success',
+                title: 'Success',
+                text: 'User has been updated',
+                confirmButtonColor: '#155e75',
+            }).then(() => {
+                router.push({ name: 'UserListing' })
+            })
+        } else {
+            const errorData = await response.json();
+            Swal.fire({
+                icon: 'error',
+                title: 'Error ' + errorData.status,
+                text: errorData.message,
+                confirmButtonColor: '#155e75',
+            })
+        }
+    } else {
+        const response = await fetch(import.meta.env.VITE_ROOT_API + "/api/users", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+
+        if (response.ok) {
+            Swal.fire({
+                icon: 'success',
+                title: 'Success',
+                text: 'User has been added',
+                confirmButtonColor: '#155e75',
+            }).then(() => {
+                router.push({ name: 'UserListing' })
+            })
+        } else {
+            const errorData = await response.json();
+            Swal.fire({
+                icon: 'error',
+                title: 'Error ' + errorData.status,
+                text: errorData.message,
+                confirmButtonColor: '#155e75',
+            })
+        }
+    }
+}
 </script>
 <template>
     <div class="w-full h-screen bg-slate-100">
@@ -65,29 +130,30 @@ const showBackButtonConfirmation = () => {
                     <div class="text-4xl mt-4">User Detail:</div>
                     <div class="my-4">
                         <div class="text-lg text-black mt-4">Username</div>
-                        <input type="text" class="border rounded-lg mt-3 pl-3 w-full h-12 bg-white" v-model.trim="user.username">
+                        <input type="text" class="ann-username border rounded-lg mt-3 pl-3 w-full h-12 bg-white" v-model.trim="user.username">
                         <div class="text-lg text-black mt-4">Name</div>
-                        <input type="text" class="border rounded-lg mt-3 pl-3 w-full h-12 bg-white" v-model.trim="user.name" maxlength="100">
+                        <input type="text" class="ann-name border rounded-lg mt-3 pl-3 w-full h-12 bg-white" v-model.trim="user.name" maxlength="100">
                         <div class="text-lg text-black mt-4">Email</div>
-                        <input type="email" class="border rounded-lg mt-3 pl-3 w-full h-12 bg-white" v-model.trim="user.email" maxlength="150">
+                        <input type="email" class="ann-email border rounded-lg mt-3 pl-3 w-full h-12 bg-white" v-model.trim="user.email" maxlength="150">
                         <div class="text-lg text-black mt-4">Role</div>
-                        <select class="select select-bordered bg-white mt-3" v-model="user.role">
+                        <select class="ann-role select select-bordered bg-white mt-3" v-model="user.role">
                             <option>admin</option>
                             <option>announcer</option>
                         </select>
                         <div class="flex mt-4" v-if="!isAddUserPage">
-                            <div class="mr-10">
+                            <div class="ann-created-on mr-10">
                                 <span class="font-bold mr-3">Created On</span>  
-                                {{ formatDatetimeLocal(user.createdOn) }}
+                                {{ formatDatetimeLocalUTC(user.createdOn) }}
                             </div>
-                            <div>
+                            <div class="ann-updated-on">
                                 <span class="font-bold mr-3">Updated On</span>
-                                {{ formatDatetimeLocal(user.updatedOn) }}
+                                {{ formatDatetimeLocalUTC(user.updatedOn) }}
                             </div>
                         </div>
                         <div class="flex mt-4">
                             <button 
-                                class="text-white bg-emerald-plus hover:bg-emerald-light border-0 shadow-lg hover:scale-110 w-28 h-12 rounded-lg">
+                                class="ann-button text-white bg-emerald-plus hover:bg-emerald-light border-0 shadow-lg hover:scale-110 w-28 h-12 rounded-lg"
+                                @click="AddEditUser()">
                                 Save
                             </button>
                             <button 
