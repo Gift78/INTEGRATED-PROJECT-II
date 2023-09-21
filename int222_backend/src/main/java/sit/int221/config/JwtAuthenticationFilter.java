906153db3e18e7436aa9cb770a4d1dcc531a2a18
tana <tanasit.istg@mail.kmutt.ac.th>
2023-09-21 16:41:59 +0700

    [Backend] chore: removed un-needed code

--- int222_backend/src/main/java/sit/int221/config/JwtAuthenticationFilter.java
+++ int222_backend/src/main/java/sit/int221/config/JwtAuthenticationFilter.java
@@ -42,7 +42,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         username = JwtService.extractUsername(jwt);
 
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 
             if (JwtService.isTokenValid(jwt, userDetails)) {
                 UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
