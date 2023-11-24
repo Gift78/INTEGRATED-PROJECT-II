a18364a6cfbed1272b1a2f0bb9d0b9ba042ee7be
Thanakorn Wongklad <thanakorn.wong@kmutt.ac.th>
2023-11-24 13:05:24 +0700

    [frontend] generate unsub token1

--- int222_frontend/src/views/Unsubscribe.vue
+++ int222_frontend/src/views/Unsubscribe.vue
@@ -1,5 +1,27 @@
 <script setup>
 import unsubIcon from '../components/icons/unsubIcon.vue'
+import { useRoute } from 'vue-router';
+const { query } = useRoute();
+
+const generateUnsubToken = async () => {
+    console.log(query?.email)
+    const response = await fetch(import.meta.env.VITE_ROOT_API + `/api/subscription/unsubscribe/generate-token?email=${query?.email}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    if (response.ok) {
+        const res = await response.json()
+        // console.log(res)
+        // localStorage.setItem('unsubToken', res.token)
+        console.log("generate unsub token success")
+        console.log(res.token)
+    } else {
+        const errorData = await response.json()
+        console.log(errorData)
+    }
+}
 </script>
  
 <template>
@@ -15,8 +37,10 @@ import unsubIcon from '../components/icons/unsubIcon.vue'
                 <unsubIcon class="text-5xl text-emerald-plus" />
             </div>
             <div class="flex justify-center mt-5">
-                <button class="px-16 py-2 rounded-lg bg-emerald-plus text-white mx-2 hover:bg-emerald-light transition-colors duration-200">No</button>
-                <button class="px-16 py-2 rounded-lg bg-zinc-400 text-white mx-2 hover:bg-zinc-500 transition-colors duration-200">Yes</button>
+                <button
+                    class="px-16 py-2 rounded-lg bg-emerald-plus text-white mx-2 hover:bg-emerald-light transition-colors duration-200">No</button>
+                <button @click="generateUnsubToken"
+                    class="px-16 py-2 rounded-lg bg-zinc-400 text-white mx-2 hover:bg-zinc-500 transition-colors duration-200">Yes</button>
             </div>
         </div>
     </div>
