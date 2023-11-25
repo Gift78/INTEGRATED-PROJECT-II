7761f1e96655a66705bd46920bdeb3d7b11fe805
Thanakorn Wongklad <thanakorn.wong@kmutt.ac.th>
2023-11-25 18:15:54 +0700

    [frontend] verify unsub token, unsubscribe finish

--- int222_frontend/src/views/Unsubscribe.vue
+++ int222_frontend/src/views/Unsubscribe.vue
@@ -1,10 +1,20 @@
 <script setup>
+import { ref, onMounted } from 'vue';
+import Swal from 'sweetalert2';
 import unsubIcon from '../components/icons/unsubIcon.vue'
 import { useRoute } from 'vue-router';
+import { getCategoryById } from '../composable/getData.js'
+
 const { query } = useRoute();
 
+
+const category = ref({})
+onMounted(async () => {
+    category.value = await getCategoryById(query?.categoryId)
+})
+
+const token = ref('')
 const generateUnsubToken = async () => {
-    console.log(query?.email)
     const response = await fetch(import.meta.env.VITE_ROOT_API + `/api/subscription/unsubscribe/generate-token?email=${query?.email}`, {
         method: 'POST',
         headers: {
@@ -13,14 +23,57 @@ const generateUnsubToken = async () => {
     })
     if (response.ok) {
         const res = await response.json()
-        // console.log(res)
-        // localStorage.setItem('unsubToken', res.token)
-        console.log("generate unsub token success")
-        console.log(res.token)
+        token.value = res.token
+        unsubscribe()
     } else {
         const errorData = await response.json()
         console.log(errorData)
     }
+}
+
+const unsubscribe = async () => {
+    const response = await fetch(import.meta.env.VITE_ROOT_API + `/api/subscription/unsubscribe`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ token: token.value, categoryId: query?.categoryId })
+    })
+    if (response.ok) {
+        Swal.fire({
+            title: 'Unsubscribed!',
+            text: `You have been unsubscribed from ${category.value.categoryName} `,
+            icon: 'success',
+            confirmButtonText: 'Ok'
+        }).then(() => {
+            window.location.href = '/'
+        })
+    } else {
+        const errorData = await response.json()
+        console.log(errorData)
+    }
+}
+
+const cancelButton = ()=>{
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You will be redirected to the home page",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#155e75',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, cancel!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Cancelled!',
+            'You have been redirected to the home page',
+            'success'
+          ).then(()=>{
+              window.location.href = '/'
+          })
+        }
+      })
 }
 </script>
  
@@ -28,16 +81,16 @@ const generateUnsubToken = async () => {
     <div class="w-full flex justify-center">
         <div class="mt-56">
             <div class="text-4xl text-center">
-                Unsubscribe
+                Unsubscribe from {{ category.categoryName}}
             </div>
-            <div class="text-xl text-center">
-                are you sure you wish to unsubscribe?
+            <div class="text-xl text-center mt-5">
+                are you sure you wish to unsubscribe from this category?
             </div>
             <div class="flex justify-center">
                 <unsubIcon class="text-5xl text-emerald-plus" />
             </div>
             <div class="flex justify-center mt-5">
-                <button
+                <button @click="cancelButton"
                     class="px-16 py-2 rounded-lg bg-emerald-plus text-white mx-2 hover:bg-emerald-light transition-colors duration-200">No</button>
                 <button @click="generateUnsubToken"
                     class="px-16 py-2 rounded-lg bg-zinc-400 text-white mx-2 hover:bg-zinc-500 transition-colors duration-200">Yes</button>
